ec246a05a359325ffa8b00b6bd5c66597bcf0dce
Nuno Maia <Deonap4216@gmail.com>
2024-03-11 17:53:18 +0000

    Small change in cliente.show view

--- resources/views/cliente/show.blade.php
+++ resources/views/cliente/show.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar Cliente</title>
+    <title>Ver Cliente</title>
     @vite('resources/css/app.css')
 
 </head>

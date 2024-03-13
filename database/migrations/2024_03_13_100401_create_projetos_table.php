2cfb628bfac27a44932d779eff23b25f6839494f
Nuno Maia <Deonap4216@gmail.com>
2024-03-13 10:14:03 +0000

    Created task model

---
+++ database/migrations/2024_03_13_100401_create_projetos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projetos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->integer('cliente_id');
+            $table->string('tipo');
+            $table->date('dataLimite');
+            $table->integer('supervisor_id');
+            $table->integer('responsavel_id');
+            $table->text('obs');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projetos');
+    }
+};
